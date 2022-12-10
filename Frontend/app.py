90e1f7f61ce9136db3b24c9c13dcda7419a295ae
Deepakraj15 <deepakraj15112002@gmail.com>
2022-12-10 11:21:12 +0530

    end of project

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,request
-
+import math
 from Backend import main,classification
 
 app = Flask(__name__)
@@ -14,6 +14,10 @@ def result():
     no_tweets = request.form['no_tweets']
     main.get_tweets(query,int( no_tweets))
     negative,neutral,positive = classification.classify() 
-    print(negative,neutral,positive)
-    return render_template('index.html',data=[negative,neutral,positive])
+    n1 = math.floor( negative*100)
+    n3 = math.floor(positive*100)
+    n2 = math.floor(neutral*100)
+
+    print(n1,n2,n3)
+    return render_template('index.html',data=[n1,n2,n3])
 
